c89c3ac1ae55276d8b5ee9397ef1cd1bb81bdad9
tidecc <tidecc5@gmail.com>
2014-08-12 10:16:19 +0800

    submit HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public Class HelloWorld {
+	public String name = "ZhanShan";
+	public String add_name = "Tom";
+
+	public static void main (String[] args){
+		System.out.println("=====" + name + "======" + add_name);
+
+	}
+}
